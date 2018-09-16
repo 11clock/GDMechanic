96991214ad70365a61fa7e00f9eefe859ba51b61
11clock <justinpriester@yahoo.com>
2018-09-16 12:32:11 -0700

    add Shuffle method to Rng

--- GDMechanic/Rng.cs
+++ GDMechanic/Rng.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GDMechanic
 {
@@ -98,6 +99,23 @@ namespace GDMechanic
 		public static bool Chance(float ratio)
 		{
 			return Randf() < ratio;
+		}
+		
+		/// <summary>
+		/// Shuffles the contents of the given list.
+		/// </summary>
+		/// <param name="list"></param>
+		/// <typeparam name="T"></typeparam>
+		public static void Shuffle<T>(this IList<T> list)  
+		{  
+			int n = list.Count;  
+			while (n > 1) {  
+				n--;  
+				int k = _rand.Next(n + 1);  
+				T value = list[k];  
+				list[k] = list[n];  
+				list[n] = value;  
+			}  
 		}
 
 		private static float NextFloat(this Random rand)
